582dd316b7cd8e6d2bc57a6eb6103245cb531540
Samantha Zammit <samanthazammit98@hotmail.com>
2023-11-08 19:10:35 -0500

    rmv php message

--- script.js
+++ script.js
@@ -28,36 +28,3 @@ $(document).ready(function(){
     });
     
 });
-
-//email form submission script
-const form = document.querySelector("form");
-const statusTxt = form.querySelector(".status-msg"); // Select the status message span
-
-form.onsubmit = (e) => {
-    e.preventDefault();
-
-    statusTxt.style.display = "block";
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "message.php", true);
-
-    xhr.onload = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let response = xhr.response;
-            console.log(response);
-
-            if (response === "success") {
-                // Update statusTxt with a success message
-                statusTxt.innerText = "Message sent successfully!";
-                statusTxt.style.color = "green"; // You can style it as you like
-            } else {
-                // Handle other responses, e.g., show an error message
-                statusTxt.innerText = "Failed to send message. Please try again.";
-                statusTxt.style.color = "red"; // You can style it as you like
-            }
-        }
-    };
-
-    let formData = new FormData(form);
-    xhr.send(formData);
-};
